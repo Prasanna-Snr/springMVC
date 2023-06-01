a94ae5c371e0007b987f1f3a574d4e10ea51eb80
Prasanna Sunuwar <prasannasnr@outlook>
2023-06-01 08:00:50 +0545

    added profile

---
+++ src/main/java/com/spring/demo/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package com.spring.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ProfileController {
+	
+	
+	@GetMapping("/profile")
+	public String UserProfile() {
+		
+		
+		return "Profile";
+	}
+
+}
